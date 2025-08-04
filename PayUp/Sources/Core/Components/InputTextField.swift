783e9441e92a35e18ea7069dc004a6f32754164e
Diogo <diogoteixeiralucas@pm.me>
2025-08-04 21:26:42 +0100

    Add custom input text field component

---
+++ PayUp/Sources/Core/Components/InputTextField.swift
@@ -0,0 +1,26 @@
+//
+//  InputTextField.swift
+//  PayUp
+//
+//  Created by Diogo on 04/08/2025.
+//
+
+import Foundation
+import UIKit
+
+final class InputTextField: UIView {
+    
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        return label
+    }()
+    
+    
+    private let textField: UITextField = {
+        let textField = UITextField()
+        textField.back
+        return textField
+    }()
+}
